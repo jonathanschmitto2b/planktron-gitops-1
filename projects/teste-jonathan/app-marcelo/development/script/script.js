deb19fcc46f28f5692b2c04fbe8586646a1ebbbd
jonathanschmitto2b <116752668+jonathanschmitto2b@users.noreply.github.com>
2023-01-25 18:22:44 -0300

    planktron-gitops: upsert script/script.js

---
+++ projects/teste-jonathan/app-marcelo/development/script/script.js
@@ -0,0 +1,4 @@
+document.getElementById("title").innerHTML = "Mudando meu HTML via JS"
+document.getElementsByClassName("subtitle")[0].innerHTML = "Mudando meu subtitle via JS"
+document.getElementsByClassName("subtitle")[1].innerHTML = "Terceiro titulo da página";
+document.getElementsByTagName("button")[0].innerHTML = "Send Infos";
